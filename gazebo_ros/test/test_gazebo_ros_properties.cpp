c0b8056d2ae14c33e77351df203a81b7afdd823f
nzlz <nestor94gonzalez@gmail.com>
2019-01-16 12:16:27 +0700

    Add get model properties test

--- gazebo_ros/test/test_gazebo_ros_properties.cpp
+++ gazebo_ros/test/test_gazebo_ros_properties.cpp
@@ -149,35 +149,9 @@ void GazeboRosPropertiesTest::SetUp()
 
 }
 
-/*void GazeboRosPropertiesTest::GetModelProperties(
+void GazeboRosPropertiesTest::GetModelProperties(
     const std::string & _name)
 {
-  // Spawn a entity using the gazebo SpawnEntity service.
-  auto spawn_request = std::make_shared<gazebo_msgs::srv::SpawnEntity::Request>();
-
-  geometry_msgs::Pose model_pose_;
-  model_pose_->position->x = 0.0;
-  model_pose_->position->y = 0.0;
-  model_pose_->position->z = 0.0;
-  model_pose_->orientation->x = 0.0;
-  model_pose_->orientation->y= 0.0;
-  model_pose_->orientation->z = 0.0;
-  model_pose_->orientation->w = 0.0;
-
-  // simple_arm model from public gazebo/models
-  spawn_request->model_name = "simple_arm";
-  spawn_request->xml ="<model name='simple_arm'> <link name='arm_base'> <inertial> <pose frame=''>0 0 0.00099 0 -0 0</pose> <inertia> <ixx>1.11</ixx> <ixy>0</ixy> <ixz>0</ixz> <iyy>100.11</iyy> <iyz>0</iyz> <izz>1.01</izz> </inertia> <mass>101</mass> </inertial> <collision name='arm_base_geom'> <pose frame=''>0 0 0.05 0 -0 0</pose> <geometry> <box> <size>1 1 0.1</size> </box> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_base_geom_visual'> <pose frame=''>0 0 0.05 0 -0 0</pose> <geometry> <box> <size>1 1 0.1</size> </box> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Blue</name> </script> </material> </visual> <collision name='arm_base_geom_arm_trunk'> <pose frame=''>0 0 0.6 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>1</length> </cylinder> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_base_geom_arm_trunk_visual'> <pose frame=''>0 0 0.6 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>1</length> </cylinder> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Red</name> </script> </material> </visual> <self_collide>0</self_collide> <enable_wind>0</enable_wind> <kinematic>0</kinematic> </link> <link name='arm_shoulder_pan'> <pose frame=''>0 0 1.1 0 -0 0</pose> <inertial> <pose frame=''>0.045455 0 0 0 -0 0</pose> <inertia> <ixx>0.011</ixx> <ixy>0</ixy> <ixz>0</ixz> <iyy>0.0225</iyy> <iyz>0</iyz> <izz>0.0135</izz> </inertia> <mass>1.1</mass> </inertial> <collision name='arm_shoulder_pan_geom'> <pose frame=''>0 0 0.05 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>0.1</length> </cylinder> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_shoulder_pan_geom_visual'> <pose frame=''>0 0 0.05 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>0.1</length> </cylinder> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Yellow</name> </script> </material> </visual> <collision name='arm_shoulder_pan_geom_arm_shoulder'> <pose frame=''>0.55 0 0.05 0 -0 0</pose> <geometry> <box> <size>1 0.05 0.1</size> </box> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_shoulder_pan_geom_arm_shoulder_visual'> <pose frame=''>0.55 0 0.05 0 -0 0</pose> <geometry> <box> <size>1 0.05 0.1</size> </box> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Yellow</name> </script> </material> </visual> <self_collide>0</self_collide> <enable_wind>0</enable_wind> <kinematic>0</kinematic> </link> <link name='arm_elbow_pan'> <pose frame=''>1.05 0 1.1 0 -0 0</pose> <inertial> <pose frame=''>0.0875 0 0.083333 0 -0 0</pose> <inertia> <ixx>0.031</ixx> <ixy>0</ixy> <ixz>0.005</ixz> <iyy>0.07275</iyy> <iyz>0</iyz> <izz>0.04475</izz> </inertia> <mass>1.2</mass> </inertial> <collision name='arm_elbow_pan_geom'> <pose frame=''>0 0 0.1 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>0.2</length> </cylinder> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_elbow_pan_geom_visual'> <pose frame=''>0 0 0.1 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>0.2</length> </cylinder> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Red</name> </script> </material> </visual> <collision name='arm_elbow_pan_geom_arm_elbow'> <pose frame=''>0.3 0 0.15 0 -0 0</pose> <geometry> <box> <size>0.5 0.03 0.1</size> </box> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_elbow_pan_geom_arm_elbow_visual'> <pose frame=''>0.3 0 0.15 0 -0 0</pose> <geometry> <box> <size>0.5 0.03 0.1</size> </box> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Yellow</name> </script> </material> </visual> <collision name='arm_elbow_pan_geom_arm_wrist'> <pose frame=''>0.55 0 0.15 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>0.3</length> </cylinder> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_elbow_pan_geom_arm_wrist_visual'> <pose frame=''>0.55 0 0.15 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>0.3</length> </cylinder> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Red</name> </script> </material> </visual> <self_collide>0</self_collide> <enable_wind>0</enable_wind> <kinematic>0</kinematic> </link> <link name='arm_wrist_lift'> <pose frame=''>1.6 0 1.05 0 -0 0</pose> <inertial> <pose frame=''>0 0 0 0 -0 0</pose> <inertia> <ixx>0.01</ixx> <ixy>0</ixy> <ixz>0</ixz> <iyy>0.01</iyy> <iyz>0</iyz> <izz>0.001</izz> </inertia> <mass>0.1</mass> </inertial> <collision name='arm_wrist_lift_geom'> <pose frame=''>0 0 0.5 0 -0 0</pose> <geometry> <cylinder> <radius>0.03</radius> <length>1</length> </cylinder> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_wrist_lift_geom_visual'> <pose frame=''>0 0 0.5 0 -0 0</pose> <geometry> <cylinder> <radius>0.03</radius> <length>1</length> </cylinder> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Yellow</name> </script> </material> </visual> <self_collide>0</self_collide> <enable_wind>0</enable_wind> <kinematic>0</kinematic> </link> <link name='arm_wrist_roll'> <pose frame=''>1.6 0 1 0 -0 0</pose> <inertial> <pose frame=''>0 0 0 0 -0 0</pose> <inertia> <ixx>0.01</ixx> <ixy>0</ixy> <ixz>0</ixz> <iyy>0.01</iyy> <iyz>0</iyz> <izz>0.001</izz> </inertia> <mass>0.1</mass> </inertial> <collision name='arm_wrist_roll_geom'> <pose frame=''>0 0 0.025 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>0.05</length> </cylinder> </geometry> <max_contacts>10</max_contacts> <surface> <contact> <ode/> </contact> <bounce/> <friction> <torsional> <ode/> </torsional> <ode/> </friction> </surface> </collision> <visual name='arm_wrist_roll_geom_visual'> <pose frame=''>0 0 0.025 0 -0 0</pose> <geometry> <cylinder> <radius>0.05</radius> <length>0.05</length> </cylinder> </geometry> <material> <script> <uri>file://media/materials/scripts/gazebo.material</uri> <name>Gazebo/Red</name> </script> </material> </visual> <self_collide>0</self_collide> <enable_wind>0</enable_wind> <kinematic>0</kinematic> </link> <joint name='arm_shoulder_pan_joint' type='revolute'> <parent>arm_base</parent> <child>arm_shoulder_pan</child> <axis> <dynamics> <damping>1</damping> <friction>0</friction> <spring_reference>0</spring_reference> <spring_stiffness>0</spring_stiffness> </dynamics> <xyz>0 0 1</xyz> <use_parent_model_frame>1</use_parent_model_frame> <limit> <lower>-1e+16</lower> <upper>1e+16</upper> </limit> </axis> </joint> <joint name='arm_elbow_pan_joint' type='revolute'> <parent>arm_shoulder_pan</parent> <child>arm_elbow_pan</child> <axis> <dynamics> <damping>1</damping> <friction>0</friction> <spring_reference>0</spring_reference> <spring_stiffness>0</spring_stiffness> </dynamics> <xyz>0 0 1</xyz> <use_parent_model_frame>1</use_parent_model_frame> <limit> <lower>-1e+16</lower> <upper>1e+16</upper> </limit> </axis> </joint> <joint name='arm_wrist_lift_joint' type='prismatic'> <parent>arm_elbow_pan</parent> <child>arm_wrist_lift</child> <axis> <dynamics> <damping>1</damping> <friction>0</friction> <spring_reference>0</spring_reference> <spring_stiffness>0</spring_stiffness> </dynamics> <limit> <lower>-0.8</lower> <upper>0.1</upper> </limit> <xyz>0 0 1</xyz> <use_parent_model_frame>1</use_parent_model_frame> </axis> </joint> <joint name='arm_wrist_roll_joint' type='revolute'> <parent>arm_wrist_lift</parent> <child>arm_wrist_roll</child> <axis> <dynamics> <damping>1</damping> <friction>0</friction> <spring_reference>0</spring_reference> <spring_stiffness>0</spring_stiffness> </dynamics> <limit> <lower>-2.99999</lower> <upper>2.99999</upper> </limit> <xyz>0 0 1</xyz> <use_parent_model_frame>1</use_parent_model_frame> </axis> </joint> <pose frame=''>0.256397 -2.58283 0 0 -0 0</pose> </model>";
-  spawn_request->robot_namespace = "";
-  spawn_request->initial_pose = model_pose_;
-  spawn_request->reference_frame = "";
-
-  auto spawn_entity_client = node->create_client<std_srvs::srv::Empty>("spawn_entity");
-  ASSERT_NE(nullptr, spawn_entity_client);
-  EXPECT_TRUE(spawn_entity_client->wait_for_service(std::chrono::seconds(1)));
-
-  auto response_future = spawn_entity_client->async_send_request(spawn_request);
-  EXPECT_EQ(rclcpp::executor::FutureReturnCode::SUCCESS,
-    rclcpp::spin_until_future_complete(node_, response_future));
 
   // Get spawned Model properties
   auto entity = world_->EntityByName(_entity);
@@ -194,16 +168,16 @@ void GazeboRosPropertiesTest::SetUp()
   ASSERT_NE(nullptr, response);
   EXPECT_TRUE(response->success);
 
-  
-  EXPECT_EQ(response->parent_model_name, )
-  EXPECT_EQ(response->canonical_body_name, )
-  EXPECT_EQ(response->body_names, )
-  EXPECT_EQ(response->geom_names, )
-  EXPECT_EQ(response->joint_names, )
-  EXPECT_EQ(response->child_model_names, )
-  EXPECT_EQ(response->is_static, )
+  // gazebo models simple_arm
+  EXPECT_EQ(response->parent_model_name, '');
+  EXPECT_EQ(response->canonical_body_name, '');
+  EXPECT_EQ(response->body_names, ['arm_base', 'arm_shoulder_pan', 'arm_elbow_pan', 'arm_wrist_lift', 'arm_wrist_roll']);
+  EXPECT_EQ(response->geom_names, ['arm_base_geom', 'arm_base_geom_arm_trunk', 'arm_shoulder_pan_geom', 'arm_shoulder_pan_geom_arm_shoulder', 'arm_elbow_pan_geom', 'arm_elbow_pan_geom_arm_elbow', 'arm_elbow_pan_geom_arm_wrist', 'arm_wrist_lift_geom', 'arm_wrist_roll_geom']);
+  EXPECT_EQ(response->joint_names, []);
+  EXPECT_EQ(response->child_model_names);
+  EXPECT_FALSE(response->is_static);
 
-}*/
+}
 
 /*void GazeboRosPropertiesTest::GetJointProperties()
 
